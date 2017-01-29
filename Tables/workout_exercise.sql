f42e55c7c96c1ac91aa133c5125c7d600ab80d0e
DrewTbay <drew.tl.reid@gmail.com>
2017-01-28 21:33:26 -0500

    Re-add the seq to the workout_exercise table

--- Tables/workout_exercise.sql
+++ Tables/workout_exercise.sql
@@ -1,4 +1,14 @@
-﻿-- Table: public.workout_exercise
+﻿
+CREATE SEQUENCE public.workout_exercise_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.workout_exercise_id_seq
+  OWNER TO postgres;
+
+-- Table: public.workout_exercise
 
 -- DROP TABLE public.workout_exercise;
 
