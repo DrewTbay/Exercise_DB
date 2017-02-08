0cd5d9bbd003c623689a43217c31295ec7e758fe
DrewTbay <drew.tl.reid@gmail.com>
2017-02-07 23:16:20 -0500

    Change the name to be less awkward

--- Tables/login_attempts.sql
+++ Tables/login_attempts.sql
@@ -1,29 +1,28 @@
--- Sequence: public.user_login_id_seq
+﻿-- Sequence: public.user_login_attempt_id_seq
 
--- DROP SEQUENCE public.user_login_id_seq;
+-- DROP SEQUENCE public.user_login_attempt_id_seq;
 
-CREATE SEQUENCE public.user_login_id_seq
+CREATE SEQUENCE public.login_attempt_id_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
-ALTER TABLE public.user_login_id_seq
+ALTER TABLE public.login_attempt_id_seq
   OWNER TO postgres;
 
+-- Table: public.login_attempts
 
--- Table: public.user_login_log
+-- DROP TABLE public.login_attempts;
 
--- DROP TABLE public.user_login_log;
-
-CREATE TABLE public.user_login_log
+CREATE TABLE public.login_attempts
 (
-  user_login_id integer NOT NULL DEFAULT nextval('user_login_id_seq'::regclass),
+  login_attempt_id integer NOT NULL DEFAULT nextval('login_attempt_id_seq'::regclass),
   attempt_date timestamp with time zone NOT NULL DEFAULT now(),
   successful_attempt boolean NOT NULL DEFAULT false,
   token text,
   user_id integer NOT NULL,
-  CONSTRAINT user_login_id PRIMARY KEY (user_login_id),
+  CONSTRAINT login_attempt_id PRIMARY KEY (login_attempt_id),
   CONSTRAINT user_fkey FOREIGN KEY (user_id)
       REFERENCES public.users (user_id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
@@ -31,7 +30,7 @@ CREATE TABLE public.user_login_log
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public.user_login_log
+ALTER TABLE public.login_attempts
   OWNER TO postgres;
 
 -- Index: public.fki_user_fkey
@@ -39,7 +38,7 @@ ALTER TABLE public.user_login_log
 -- DROP INDEX public.fki_user_fkey;
 
 CREATE INDEX fki_user_fkey
-  ON public.user_login_log
+  ON public.login_attempts
   USING btree
   (user_id);
 
