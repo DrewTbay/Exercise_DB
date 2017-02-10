5927be496b3e773537069b9330fcdebd6d1b5848
DrewTbay <drew.tl.reid@gmail.com>
2017-02-09 22:37:57 -0500

    Remove useless primary key

--- Tables/schedule_log.sql
+++ Tables/schedule_log.sql
@@ -1,29 +1,14 @@
--- Sequence: public.schedule_log_id_seq
-
--- DROP SEQUENCE public.schedule_log_id_seq;
-
-CREATE SEQUENCE public.schedule_log_id_seq
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE public.schedule_log_id_seq
-  OWNER TO postgres;
-
-
--- Table: public.schedule_log
+﻿-- Table: public.schedule_log
 
 -- DROP TABLE public.schedule_log;
 
 CREATE TABLE public.schedule_log
 (
-  schedule_log_id integer NOT NULL DEFAULT nextval('schedule_log_id_seq'::regclass),
   user_id integer NOT NULL,
   current_week integer NOT NULL DEFAULT 1,
   current_day integer NOT NULL DEFAULT 1,
   schedule_id integer NOT NULL,
-  CONSTRAINT schedule_log_pkey PRIMARY KEY (schedule_log_id),
+  CONSTRAINT schedule_log_pkey PRIMARY KEY (user_id, schedule_id),
   CONSTRAINT schedule_fkey FOREIGN KEY (schedule_id)
       REFERENCES public.schedule (schedule_id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE,
